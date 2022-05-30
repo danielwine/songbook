de642aa9aacb33787da8f36a31134d70727535c7
danielwine <danielwine@tutamail.com>
2022-05-30 12:49:03 +0200

    Frontend: create project structure. add basic navigation bar.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,10 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './common/header/header.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
