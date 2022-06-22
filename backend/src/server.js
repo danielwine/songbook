18cf1f258d160e98f4361465735514967c9f7cff
danielwine <danielwine@tutamail.com>
2022-06-22 19:41:45 +0200

    Backend: create initial swagger yaml file. fix package.json. add static file serving.

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,6 +37,7 @@ app.use(morgan('tiny', { stream: logger.stream }));
 
 app.use(cors());
 app.use(express.json())
+app.use(express.static('public')); 
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
